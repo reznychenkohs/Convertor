87bd9ae6c645bcfd5b329040df7637a81af666a1
MSI\Anna <nysya-2802@gmail.com>
2025-12-16 00:23:43 +0200

    added conversion of kilometers to miles

--- src/App/Main.java
+++ src/App/Main.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Converter project initialized!");
+        System.out.println("Version: 1.0");
 
         System.out.print("Enter miles: ");
-        Scanner scanner = new Scanner(System.in);
         double miles = scanner.nextDouble();
         double kilometers = convertMilesToKilometers(miles);
         System.out.println(miles + " miles = " + kilometers + " kilometers");
+
+        System.out.println("Enter kilometers: ");
+        kilometers = scanner.nextDouble();
+        miles = convertKilometersToMiles(kilometers);
+        System.out.println(kilometers + " kilometers = " + miles + " miles");
+
     }
 
     public static double convertMilesToKilometers(double miles) {
         double i = 1.60934;
         return miles * i;
+    }
+
+    public static double convertKilometersToMiles(double kilometers) {
+        double i = 0.621371;
+        return kilometers * i;
     }
 }
