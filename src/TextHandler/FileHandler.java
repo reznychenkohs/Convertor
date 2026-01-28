c3637045bb50fe53aebdad621bb27d4e7c5d384b
MSI\Anna <nysya-2802@gmail.com>
2026-01-28 15:24:22 +0200

    add TextHandler

---
+++ src/TextHandler/FileHandler.java
@@ -0,0 +1,43 @@
+package TextHandler;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "C:\\Users\\Anna\\IdeaProjects\\Converter\\";
+
+    public String createFile(String path) {
+        Path newFile;
+        try {
+            newFile = Files.createFile(Path.of(path));
+        } catch (FileAlreadyExistsException e) {
+            return "File already exists!";
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+        return "Created " + newFile;
+    }
+
+    public String writeToFile(Path path, String content) {
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return "Recorded in " + path;
+    }
+
+    public String readFromFile(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+    }
+}
+
+
---
+++ src/TextHandler/Main.java
@@ -0,0 +1,25 @@
+package TextHandler;
+
+import java.nio.file.Paths;
+
+public class Main {
+
+    private static final String BASE_PATH = "C:\\Users\\Anna\\IdeaProjects\\Converter\\";
+
+    public static void main(String[] args) {
+        FileHandler handler = new FileHandler();
+        String newFileName = "myfile";
+        String content = "Super information.";
+        String path = BASE_PATH + newFileName + ".txt";
+        // Виклики методів маніпуляції з файлом
+        getOutput(handler.createFile(path));
+        getOutput(handler.writeToFile(Paths.get(path), content));
+        getOutput("CONTENT: " + handler.readFromFile(path));
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
+
+
